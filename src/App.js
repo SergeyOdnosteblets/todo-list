6524511e13da3971fc0eeb361eb3bb381098aa8f
SergeyOdnosteblets <59088833+SergeyOdnosteblets@users.noreply.github.com>
2022-08-04 22:44:14 +0300

    finished version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import "./styles.css";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { AddTodo } from './components/AddTodo/AddTodo';
+import { Todo } from './components/Todo/Todo';
 
-export default function App() {
+const getLocalStorage = () => {
+  let todos = localStorage.getItem('to-do-list');
+
+  if (todos) {
+    return (todos = JSON.parse(localStorage.getItem('to-do-list')));
+  } else {
+    return [];
+  }
+};
+
+function App() {
+  const [todos, setTodos] = useState(getLocalStorage());
+  const [update, setUpdate] = useState(true);
+
+  useEffect(() => {
+    localStorage.setItem('to-do-list', JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Todo todos={todos} setTodos={setTodos} update={update} setUpdate={setUpdate} />
+      <AddTodo index={0} todos={todos} setTodos={setTodos} update={update} setUpdate={setUpdate} />
     </div>
   );
 }
+
+export default App;
